ac7c385cbef71cc261efe926937536e84259c7a3
Shubham Nandi <shubham.nandi@incedoinc.com>
2022-02-10 21:42:53 +0530

    changes

--- src/Pages/LoginScreen.js
+++ src/Pages/LoginScreen.js
@@ -1,1 +1,40 @@
 //TODO UJjwal
+import { Button } from "bootstrap";
+import React, { Component } from "react";
+class LoginScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div className="main text-center">
+                <div className="">
+                    <h1 className="border border-3 border-dark py-3 px-5">AssetMark - Shift Allowance App</h1>
+                </div>
+                <div>
+                    <div className="">
+                        <p className="fw-bold">User ID</p>
+                        <input></input>
+                    </div>
+                    <div>
+                        <p className="fw-bold">Password</p>
+                        <input></input>
+                    </div>
+                    <button type="button" class="btn btn-light border-dark px-3 py-1 border-2">Login</button>
+                    <p>New User? Request Access</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginScreen;
